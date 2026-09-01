0888bd62714738070b2515cb55763e9505698a0f
santiagomarkanthony758-star <santiagomarkanthony758@gmail.com>
2026-09-02 00:10:50 +0800

    Add files via upload

---
+++ Activity6.py
@@ -0,0 +1,10 @@
+# First step to dynamic content is thru INPUTS (User Input)
+
+name = input("What is your name ..? ")
+
+print("Hi ", name, " How are you doin ?")
+
+okay = input("are you okay?")
+print("you answerd",  okay)
+Age = input("Age")
+#code is not dynamic - ever changing , static - fixed
