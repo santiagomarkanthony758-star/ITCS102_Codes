b0d389087334078e4c0cb162e3d7ebbbfcc53977
santiagomarkanthony758-star <santiagomarkanthony758@gmail.com>
2026-09-02 00:08:04 +0800

    Add files via upload

---
+++ activity 3..py
@@ -0,0 +1,30 @@
+# this is my first python program
+print("i like santiagomarkanthony758@gmail.com !")
+print("it's really good!")
+
+#strings
+first_name = "Mark Anthony"
+hobby = "Basketball"
+email = "santiagomarkanthony758@gmail.com"
+
+print(f"you are {first_name}")
+print(f"you like {hobby}")
+print(f"your email is {email}")
+
+#integers
+age = 18
+quantities = 3
+num_of_students = 45
+
+print(f"you are {age} years old")
+print(f"you are buying {quantities} items")
+print(f"your class has {num_of_students} students")
+
+#float
+price =  3.5
+gpa = 3.5
+distance = 6.5
+
+print(f"the price is ${price}")
+print(f"your gpa is: {gpa}")
+print(f"you run {distance}km")
